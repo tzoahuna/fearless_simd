1c9cf422ae271b51d108f99d2da6481e4b58e719
tzoahuna <26078161+tzoahuna@users.noreply.github.com>
2025-10-03 12:54:33 +0100

    Fix the use of `feature="std"` in simd_dispatch (#102)
    See also
    https://xi.zulipchat.com/#narrow/channel/514230-simd/topic/using.20the.20library/with/538033615
    
    I know this is deprecated, but it also doesn't hurt anything to fix
    this.

--- fearless_simd/src/macros.rs
+++ fearless_simd/src/macros.rs
@@ -78,7 +78,7 @@ macro_rules! simd_dispatch {
                     $inner( simd128 $( , $arg )* )
                 }
             }
-            #[cfg(all(feature = "std", any(target_arch = "x86", target_arch = "x86_64")))]
+            #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
             #[target_feature(enable = "sse4.2")]
             #[inline]
             $($unsafe)? fn inner_sse4_2(sse4_2: $crate::x86::Sse4_2 $( , $arg: $ty )* ) $( -> $ret )? {
@@ -86,7 +86,7 @@ macro_rules! simd_dispatch {
                     $inner( sse4_2 $( , $arg )* )
                 }
             }
-            #[cfg(all(feature = "std", any(target_arch = "x86", target_arch = "x86_64")))]
+            #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
             #[target_feature(enable = "avx2,fma")]
             #[inline]
             $($unsafe)? fn inner_avx2(avx2: $crate::x86::Avx2 $( , $arg: $ty )* ) $( -> $ret )? {
@@ -104,9 +104,9 @@ macro_rules! simd_dispatch {
                 $crate::Level::Neon(neon) => unsafe { inner_neon (neon $( , $arg )* ) }
                 #[cfg(all(target_arch = "wasm32", target_feature = "simd128"))]
                 $crate::Level::WasmSimd128(wasm) => unsafe { inner_wasm_simd128 (wasm $( , $arg )* ) }
-                #[cfg(all(feature = "std", any(target_arch = "x86", target_arch = "x86_64")))]
+                #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
                 $crate::Level::Sse4_2(sse4_2) => unsafe { inner_sse4_2(sse4_2 $( , $arg)* ) }
-                #[cfg(all(feature = "std", any(target_arch = "x86", target_arch = "x86_64")))]
+                #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
                 $crate::Level::Avx2(avx2) => unsafe { inner_avx2(avx2 $( , $arg)* ) }
                 _ => unreachable!()
             }
